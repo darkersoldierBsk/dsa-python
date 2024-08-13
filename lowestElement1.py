fbb565467a8d4782d4bbcc96df5e401119b71e0c
basaike <134071847+darkersoldierBsk@users.noreply.github.com>
2024-08-13 03:26:23 +0300

    lowestElement1.py

---
+++ lowestElement1.py
@@ -0,0 +1,9 @@
+#Lowest in an Array
+
+#TIP1 - INDEXING
+arr = [2, 8, 12, 100]
+minVal = arr[0]      # Step 1 - Create a min.value var and set it to the first element of the array
+
+for i in arr:        # Step 2 - Go through every element of the array
+    if i < minVal:   # Step 3 - If the current element is lower than the min. value, set current as the minVal
+        minVal = i
